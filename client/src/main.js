bf8c86446e97c2ed7793e43ea0ab7d4f1e086378
douglas de lima bezerra <douglaslima13bezerra@gmail.com>
2025-06-28 19:29:28 -0300

    feat (client): add navbar

--- client/src/main.js
+++ client/src/main.js
@@ -1,1 +1,19 @@
+const menu = document.querySelector('.menu');
+const navList = document.querySelector('.nav-list');
+const navItems = document.querySelectorAll('.nav-item');
+const navLinks = document.querySelectorAll('.nav-item a');
+
+menu.addEventListener('click', () => {
+    navList.classList.toggle('open');
+});
+
+navLinks.forEach((link) => {
+    link.addEventListener('click', (e) => {
+        navItems.forEach((item) => item.classList.remove('active'));
+
+        link.parentElement.classList.add('active');
+        navList.classList.remove('open');
+    });
+});
+
 lucide.createIcons();
